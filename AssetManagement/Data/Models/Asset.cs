c82f42554280e6f4822722e465a12362c025403e
Aemon33 <khazriaymen93@gmail.com>
2025-03-28 18:23:19 +0100

    add a model, get rid of nullable

---
+++ AssetManagement/Data/Models/Asset.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AssetManagement.Data.Models
+{
+    public class Asset
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double UnitPrice { get; set; }
+        public DateTime DateOfPurchase { get; set; }
+        public string Status { get; set; }
+        public string AssignedTo { get; set; }
+        public DateTime AssignmentDate { get; set; }
+        public DateTime ReplacementDate { get; set; }
+        public string Department { get; set; }
+
+
+
+    }
+}
